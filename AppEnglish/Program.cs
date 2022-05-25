040e40dd75e403b41070dd04990d971055ee06e0
Denis <den.zhernov@yandex.ru>
2022-05-25 11:08:15 +0300

    добавлен функционал ввода и проверки слов

--- AppEnglish/Program.cs
+++ AppEnglish/Program.cs
@@ -6,7 +6,25 @@ namespace AppEnglish
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var tutor = new Tutor();
+			tutor.AddWord("dog", "пес");
+			tutor.AddWord("cat", "кот");
+			tutor.AddWord("black", "черный");
+
+			while (true)
+			{
+				var word = tutor.GetRandomEngWord();
+				Console.WriteLine($"Как переводится слово \"{word}\"?");
+				var userAnswer = Console.ReadLine();
+
+				if (tutor.CheckWord(word, userAnswer))
+					Console.WriteLine("Правильно!");
+				else
+				{
+					var correctAnswer = tutor.Translate(word);
+					Console.WriteLine($"Не правильно. Правильный ответ: {correctAnswer}");
+				}
+			}
 		}
 	}
 }
---
+++ AppEnglish/Tutor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppEnglish
+{
+	internal class Tutor
+	{
+		private Dictionary<string, string> _dic = new Dictionary<string, string>();
+		private Random _random = new Random();
+		public void AddWord(string eng, string rus)
+		{
+			_dic.Add(eng, rus);
+		}
+
+		public bool CheckWord(string eng, string rus)
+		{
+			var answer = _dic[eng];
+			return answer.ToLower() == rus;
+		}
+
+		public string Translate(string eng)
+		{
+			if (_dic.ContainsKey(eng))
+			{
+				return _dic[eng];
+			}
+			
+			return null;
+		}
+
+		public string GetRandomEngWord()
+		{
+			var r = _random.Next(0, _dic.Count);
+			var keys = new List<string>(_dic.Keys);
+			return keys[r];
+		}
+	}
+}
